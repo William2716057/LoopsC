b36e9d53209d09a5c1ae8f13dbc701e76e8a03da
William Spriggs <Willspri@gmail.com>
2023-10-31 16:20:28 +1300

    changed

--- LoopsC/LoopsC.cpp
+++ LoopsC/LoopsC.cpp
@@ -17,6 +17,18 @@ int main() {
     }
 
 
+    for (int i = 0; i < 6; i++) {
+        std::string x = "* ";
+        x = x + std::string(i, '*');
+        std::cout << x << std::string(10 - x.length(), ' ') << std::endl;
+    }
+
+
+    for (int i = 0; i < 6; i++) {
+        std::string x = "* ";
+        x = x + std::string(i, '*');
+        std::cout << std::string(10 - x.length(), ' ') << x << std::endl;
+    }
 
     return 0;
 }
